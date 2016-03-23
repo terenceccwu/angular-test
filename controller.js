37f28442a07e849474d441452e4bc027a7f84811
Terence Wu <terenceccwu@gmail.com>
2016-03-23 11:11:45 +0800

    updating map

--- controller.js
+++ controller.js
@@ -1,73 +1,78 @@
-var gmap = angular.module('gmap', [])
-.controller('MapController', ['$scope',function ($scope) {
+var gmap = angular.module('gmap', ['gservice'])
+.controller('MapController', ['$scope', 'gservice', '$rootScope',function ($scope, gservice, $rootScope) {
   var self = this;
-  this.lat = 22.3782353;
-  this.lng = 114.1807784;
+  this.lat = 0;
+  this.lng = 0;
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: {lat: this.lat, lng: this.lng}
+  $rootScope.$on('gservice:centerChanged', function (event,arg) {
+    self.lat  = arg.lat;
+    self.lng  = arg.lng;
   });
 
-  map.addListener('click',function (event) {
-    self.lat = event.latLng.lat();
-    self.lng = event.latLng.lng();
-    $scope.$apply();
-  });
-
-
-  var service = new google.maps.places.PlacesService(map);
-  this.search = function () {
-    var request = {
-      bounds: map.getBounds(),
-      type: ['store']
-    }
-    service.nearbySearch(request, function(results, status) {
-        console.log(results);
-        results.forEach(function(r) {
-          console.log(r.name);
-          createMarker(r);
-        });
-      });
-  }
-
-
-  this.places = [];
-  
-  var markers = [];
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location,
-
-    });
-    var infowindow = new google.maps.InfoWindow();
-
-    google.maps.event.addListener(marker, 'mouseover', function() {
-      infowindow.setContent("<b>" +place.name + "</b>");
-      infowindow.open(map, this);
-    });
-    google.maps.event.addListener(marker, 'mouseout', function() {
-      infowindow.close();
-    });
-    google.maps.event.addListener(marker, 'dblclick', function() {
-      self.places.push(place.name);
-      $scope.$apply();
-    });
-
-    markers.push(marker);
-  }
-
-  this.delete = function () {
-    markers.forEach(function (m) {
-      console.log(123);
-      m.setMap(null);
-    });
-    markers = [];
-  }
-
-  this.save = function () {
-    window.alert(this.places);
-  }
+  // var map = new google.maps.Map(document.getElementById('map'), {
+  //     zoom: 10,
+  //     center: {lat: this.lat, lng: this.lng}
+  // });
+  //
+  // map.addListener('click',function (event) {
+  //   self.lat = event.latLng.lat();
+  //   self.lng = event.latLng.lng();
+  //   $scope.$apply();
+  // });
+  //
+  //
+  // var service = new google.maps.places.PlacesService(map);
+  // this.search = function () {
+  //   var request = {
+  //     bounds: map.getBounds(),
+  //     type: ['store']
+  //   }
+  //   service.nearbySearch(request, function(results, status) {
+  //       console.log(results);
+  //       results.forEach(function(r) {
+  //         console.log(r.name);
+  //         createMarker(r);
+  //       });
+  //     });
+  // }
+  //
+  //
+  // this.places = [];
+  //
+  // var markers = [];
+  // function createMarker(place) {
+  //   var placeLoc = place.geometry.location;
+  //   var marker = new google.maps.Marker({
+  //     map: map,
+  //     position: place.geometry.location,
+  //
+  //   });
+  //   var infowindow = new google.maps.InfoWindow();
+  //
+  //   google.maps.event.addListener(marker, 'mouseover', function() {
+  //     infowindow.setContent("<b>" +place.name + "</b>");
+  //     infowindow.open(map, this);
+  //   });
+  //   google.maps.event.addListener(marker, 'mouseout', function() {
+  //     infowindow.close();
+  //   });
+  //   google.maps.event.addListener(marker, 'dblclick', function() {
+  //     self.places.push(place.name);
+  //     $scope.$apply();
+  //   });
+  //
+  //   markers.push(marker);
+  // }
+  //
+  // this.delete = function () {
+  //   markers.forEach(function (m) {
+  //     console.log(123);
+  //     m.setMap(null);
+  //   });
+  //   markers = [];
+  // }
+  //
+  // this.save = function () {
+  //   window.alert(this.places);
+  // }
 }]);
--- gservice.js
+++ gservice.js
@@ -1,74 +1,20 @@
 // Creates the gservice factory. This will be the primary means by which we interact with Google Maps
 angular.module('gservice', [])
-    .factory('gservice', function(){
+    .factory('gservice', function($rootScope){
+      var gmap = {};
 
-        // Initialize Variables
-        // -------------------------------------------------------------
-        // Service our factory will return
-        var googleMapService = {};
-
-        // Array of locations obtained from API calls
-        var locations = [];
-
-        // Selected Location (initialize to center of America)
-        var selectedLat  = 22.3782353;
-        var selectedLong = 114.1807784;
-
-        var map;
-
-        googleMapService.lat = 0;
-        googleMapService.lng = 0;
-
-        // Functions
-        // --------------------------------------------------------------
-        // Refresh the Map with new data. Function will take new latitude and longitude coordinates.
-        googleMapService.init = function(latitude, longitude){
-
-            // Clears the holding array of locations
-            locations = [];
-
-            // Set the selected lat and long equal to the ones provided on the refresh() call
-            selectedLat = latitude;
-            selectedLong = longitude;
-
-            initialize(latitude, longitude);
-
-        };
-
-        googleMapService.refresh = function (lat, lng) {
-          map.setCenter({lat: lat, lng: lng});
-        };
-
-        // Initializes the map
-        var initialize = function(latitude, longitude) {
-
-          // Uses the selected lat, long as starting point
-          var myLatLng = {lat: latitude, lng: longitude};
-
-          // If map has not been created already...
-          if (!map){
-
-              // Create a new map and place in the index.html page
-              map = new google.maps.Map(document.getElementById('map'), {
-                  zoom: 10,
-                  center: myLatLng
-              });
-              console.log('created map');
-          };
-
-        };
-
-        googleMapService.query = function () {
-
-        }
-
+      var map; //google map Object
+      gmap.init = function (lat, lng) {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 10,
+            center: {lat: lat, lng: lng}
+        });
+        $rootScope.$emit('gservice:centerChanged', {lat: lat, lng: lng});
+      }
 
         // Refresh the page upon window load. Use the initial latitude and longitude
-        google.maps.event.addDomListener(window, 'load',
-            googleMapService.init(selectedLat, selectedLong));
+      google.maps.event.addDomListener(window, 'load',
+          gmap.init(22.3782353, 114.180778));
 
-
-        googleMapService.map = map;
-
-        return googleMapService;
+      return gmap;
 });
